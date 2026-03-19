93ce2f9be028eaeeca24fa228613d6722cf250c3
Thao <thao@email.com>
2026-03-19 15:02:13 +0700

    [TECH] Root: Infrastructure Scaffolding & Solution Setup #1

---
+++ BazanAI/src/Services/BazanAI.Agronomy/BazanAI.Agronomy.API/Program.cs
@@ -0,0 +1,1 @@
+var builder = WebApplication.CreateBuilder(args); var app = builder.Build(); app.MapGet("/", () => "Agronomy API"); app.Run();
---
+++ BazanAI/src/Services/BazanAI.Orchestrator/BazanAI.Orchestrator/Program.cs
@@ -0,0 +1,1 @@
+var builder = WebApplication.CreateBuilder(args); var app = builder.Build(); app.MapGet("/", () => "Orchestrator"); app.Run();
